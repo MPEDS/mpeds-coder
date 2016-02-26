40473b31176fdfcfb359e852201ce1933f3d00db
yijingbai <everatnight@gmail.com>
2016-02-26 01:50:41 -0600

    adding paging feature

--- mpeds_coder.py
+++ mpeds_coder.py
@@ -12,6 +12,7 @@
 import csv, json, math, os, re, string, sys, urllib2
 import datetime as dt
 import time
+from math import ceil
 
 ## pandas
 import pandas as pd
@@ -476,19 +477,153 @@ def code2(aid):
 ##### Coder 2 queue
 #####
 
+class Pagination(object):
+    """
+    Extracted from flask-sqlalchemy
+    Internal helper class returned by :meth:`BaseQuery.paginate`.  You
+    can also construct it from any other SQLAlchemy query object if you are
+    working with other libraries.  Additionally it is possible to pass `None`
+    as query object in which case the :meth:`prev` and :meth:`next` will
+    no longer work.
+    """
+
+    def __init__(self, query, page, per_page, total, items):
+        #: the unlimited query object that was used to create this
+        #: pagination object.
+        self.query = query
+        #: the current page number (1 indexed)
+        self.page = page
+        #: the number of items to be displayed on a page.
+        self.per_page = per_page
+        #: the total number of items matching the query
+        self.total = total
+        #: the items for the current page
+        self.items = items
+
+    @property
+    def pages(self):
+        """The total number of pages"""
+        if self.per_page == 0:
+            pages = 0
+        else:
+            pages = int(ceil(self.total / float(self.per_page)))
+        return pages
+
+    def prev(self, error_out=False):
+        """Returns a :class:`Pagination` object for the previous page."""
+        assert self.query is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return paginate(self.query, self.page - 1, self.per_page, error_out)
+
+    @property
+    def prev_num(self):
+        """Number of the previous page."""
+        return self.page - 1
+
+    @property
+    def has_prev(self):
+        """True if a previous page exists"""
+        return self.page > 1
+
+    def next(self, error_out=False):
+        """Returns a :class:`Pagination` object for the next page."""
+        assert self.query is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return paginate(self.query, self.page + 1, self.per_page, error_out)
+
+    @property
+    def has_next(self):
+        """True if a next page exists."""
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        """Number of the next page"""
+        return self.page + 1
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """Iterates over the page numbers in the pagination.  The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides.  Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+        .. sourcecode:: html+jinja
+            {% macro render_pagination(pagination, endpoint) %}
+              <div class=pagination>
+              {%- for page in pagination.iter_pages() %}
+                {% if page %}
+                  {% if page != pagination.page %}
+                    <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                  {% else %}
+                    <strong>{{ page }}</strong>
+                  {% endif %}
+                {% else %}
+                  <span class=ellipsis>…</span>
+                {% endif %}
+              {%- endfor %}
+              </div>
+            {% endmacro %}
+        """
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+
+def paginate(query, page, per_page=20, error_out=True):
+    """
+    Modified from the flask-sqlalchemy to support paging for
+    the original version of the sqlalchemy that not use BaseQuery.
+    """
+    if page < 1 and error_out:
+        abort(404)
+
+    print("per_page" + str(per_page))
+    items = query.limit(per_page).offset((page - 1) * per_page).all()
+    if not items and page != 1 and error_out:
+        abort(404)
+
+    # No need to count if we're on the first page and there are fewer
+    # items than we expected.
+    if page == 1 and len(items) < per_page:
+        total = len(items)
+    else:
+        total = query.order_by(None).count()
+
+    return Pagination(query, page, per_page, total, items)
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
 @app.route('/code2queue/<sort>/<sort_dir>')
+@app.route('/code2queue/<sort>/<sort_dir>/<int:page>')
 @login_required
-def code2queue(sort, sort_dir):
+def code2queue(sort, sort_dir, page = 1):
     if current_user.authlevel < 2:
         return redirect(url_for('index'))
 
     ## get existing queue items to note which ones are coded
-    spqs = {spq.article_id: 1 for spq in db_session.query(SecondPassQueue).filter(SecondPassQueue.coded_dt != None).all()}
+    spqs = {spq.article_id: 1 for spq in db_session.query(SecondPassQueue)
+            .filter(SecondPassQueue.coded_dt != None).all()}
 
     ## get coding info
     cfp_dict  = {}
     cfp_order = []
-    for cfp, am in db_session.query(CodeFirstPass, ArticleMetadata).join(ArticleMetadata).filter(CodeFirstPass.variable == 'protest').all():
+
+    pagination = paginate(db_session.query(CodeFirstPass, ArticleMetadata)
+                            .join(ArticleMetadata)
+                            .filter(CodeFirstPass.variable == 'protest'),
+                            page, 130, True)
+    for cfp, am in pagination.items:
         pub = ''
         if 'AGW' in am.db_id:
             pub = "-".join(am.db_id.split("_")[0:2])
@@ -538,7 +673,8 @@ def code2queue(sort, sort_dir):
         "code2queue.html",
         cfp_dict  = cfp_dict,
         cfp_order = cfp_order,
-        spqs = spqs)
+        spqs = spqs,
+        pagination = pagination)
 
 #####
 ##### Coder stats
