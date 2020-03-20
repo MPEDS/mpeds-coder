8759712f7e0276f79231dcc00a4f78f4fb36d5e9
David Skalinder <skalinder@no.spam.wisc.edu>
2020-03-20 18:44:18 -0500

    Created testing environment, working on imports

--- data-wrangling-sandbox/test.py
+++ data-wrangling-sandbox/test.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import sqlalchemy
 import sys
-sys.path.append('../')
-import config
-
-#mysql_engine = create_engine('mysql://%s:%s@localhost/%s?unix_socket=%s&charset=%s' % 
-#    (config.MYSQL_USER, config.MYSQL_PASS, config.MYSQL_DB, config.MYSQL_SOCK, 'utf8'), 
-#                                 convert_unicode=True)
-pass
+#from .. import config
+#
+#mysql_engine = sqlalchemy.create_engine('mysql://%s:%s@localhost/%s?unix_socket=%s&charset=%s' % 
+#                                        (config.MYSQL_USER, 
+#                                        config.MYSQL_PASS, 
+#                                        config.MYSQL_DB, 
+#                                        config.MYSQL_SOCK, 
+#                                        'utf8'), 
+#                                        convert_unicode=True)
+#db_session = scoped_session(sessionmaker(autocommit=False,
+#                                         autoflush=False,
+#                                         bind=mysql_engine))
+print(sys.path)
