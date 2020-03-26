f6a37750bad9d45cd68c2125a2ca60f796e8755c
David Skalinder <skalinder@no.spam.wisc.edu>
2020-03-26 12:14:10 -0500

    Got data from MySQL into Pandas

--- data_wrangling_sandbox.py
+++ data_wrangling_sandbox.py
@@ -2,18 +2,25 @@ import sys
 
 import pandas as pd
 import sqlalchemy
+import sqlalchemy.orm
 
 import config
+import models
+
+mysql_engine = sqlalchemy.create_engine(
+    'mysql://%s:%s@localhost/%s?unix_socket=%s&charset=%s' % 
+        (config.MYSQL_USER, 
+        config.MYSQL_PASS, 
+        config.MYSQL_DB, 
+        config.MYSQL_SOCK, 
+        'utf8'), 
+    convert_unicode=True)
+#db_session = sqlalchemy.orm.scoped_session(
+#    sqlalchemy.orm.sessionmaker(
+#        autocommit=False,
+#        autoflush=False,
+#        bind=mysql_engine))
 #
-#mysql_engine = sqlalchemy.create_engine('mysql://%s:%s@localhost/%s?unix_socket=%s&charset=%s' % 
-#                                        (config.MYSQL_USER, 
-#                                        config.MYSQL_PASS, 
-#                                        config.MYSQL_DB, 
-#                                        config.MYSQL_SOCK, 
-#                                        'utf8'), 
-#                                        convert_unicode=True)
-#db_session = scoped_session(sessionmaker(autocommit=False,
-#                                         autoflush=False,
-#                                         bind=mysql_engine))
-print __name__
-print '\n'.join(sys.path)
+#print(db_session.query(models.ArticleMetadata).filter_by(id = 10).first())
+df = pd.read_sql_table("user", con=mysql_engine)
+print df
