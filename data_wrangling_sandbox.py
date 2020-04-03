3881d80ca7cad1a3a5e3f7cd21bf0a4020a739db
David Skalinder <skalinder@no.spam.wisc.edu>
2020-04-03 16:52:58 -0500

    Uncommented testing code in sandbox

--- data_wrangling_sandbox.py
+++ data_wrangling_sandbox.py
@@ -23,35 +23,35 @@ sobj = data_wrangling.solr.Solr()
 sobj.setSolrURL('%s/select' % config.SOLR_ADDR)
 
 ### MySQL testing
-#user_df = pd.read_sql_table("user", con=mysql_engine)
-#print user_df
-#
-### SOLR testing
-##SEARCH_STR = 'boycott* "press conference" "news conference" (protest* AND NOT protestant*) strik* rally ralli* riot* sit-in occupation mobiliz* blockage demonstrat* marchi* marche*'
-#
-#QUERY_STR = 'PUBLICATION:("Associated Press Worldstream, English Service" OR "New York Times Newswire Service" OR ' + \
-#        '"Los Angeles Times/Washington Post Newswire Service" OR "Washington Post/Bloomberg Newswire Service")'
-#FQ_STR = 'Wisconsin AND boycott'
-#
-#output_counts = {}
-#
-#output_counts['retrieved-articles'] = sobj.getResultsFound(QUERY_STR, FQ_STR)
-#
-#print("Retrieving %d articles..." % output_counts['retrieved-articles'])
-#
-#import time
-#t0    = time.time()
-#docs  = sobj.getDocuments(QUERY_STR, fq = FQ_STR)
-#test_time = time.time() - t0
-#print("Loading time:  %0.3fs" % test_time)
-#
-#solr_df = pd.DataFrame(docs)
-#
-##print df
-##print '\n'.join([doc[u'TITLE'] for doc in docs])
-#
-#output_counts['retrieved-articles-cleaned'] = solr_df.shape[0]
-#print("Article count: %d" % solr_df.shape[0])
+user_df = pd.read_sql_table("user", con=mysql_engine)
+print user_df
+
+## SOLR testing
+#SEARCH_STR = 'boycott* "press conference" "news conference" (protest* AND NOT protestant*) strik* rally ralli* riot* sit-in occupation mobiliz* blockage demonstrat* marchi* marche*'
+
+QUERY_STR = 'PUBLICATION:("Associated Press Worldstream, English Service" OR "New York Times Newswire Service" OR ' + \
+        '"Los Angeles Times/Washington Post Newswire Service" OR "Washington Post/Bloomberg Newswire Service")'
+FQ_STR = 'Wisconsin AND boycott'
+
+output_counts = {}
+
+output_counts['retrieved-articles'] = sobj.getResultsFound(QUERY_STR, FQ_STR)
+
+print("Retrieving %d articles..." % output_counts['retrieved-articles'])
+
+import time
+t0    = time.time()
+docs  = sobj.getDocuments(QUERY_STR, fq = FQ_STR)
+test_time = time.time() - t0
+print("Loading time:  %0.3fs" % test_time)
+
+solr_df = pd.DataFrame(docs)
+
+#print df
+#print '\n'.join([doc[u'TITLE'] for doc in docs])
+
+output_counts['retrieved-articles-cleaned'] = solr_df.shape[0]
+print("Article count: %d" % solr_df.shape[0])
 
 ## Crossover testing
 
@@ -76,11 +76,11 @@ print("Loading time:  %0.3fs" % test_time)
 
 solr_df = pd.DataFrame(docs)
 
-print '\n'.join([key + ':' + str(len(val))
-                    if isinstance(val, list) 
-                    else key + ':' + str(type(val)) 
-                    for key, val in docs[3].iteritems()])
-#print solr_df
+#print '\n'.join([key + ':' + str(len(val))
+#                    if isinstance(val, list) 
+#                    else key + ':' + str(type(val)) 
+#                    for key, val in docs[3].iteritems()])
+print solr_df
 #print '\n'.join([doc[u'TITLE'] for doc in docs])
 
 output_counts['retrieved-articles-cleaned'] = solr_df.shape[0]
