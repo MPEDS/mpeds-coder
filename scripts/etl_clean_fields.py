c09f7e744c25f7c2c29687ea3470ecefb62f30b1
David Skalinder <skalinder@no.spam.wisc.edu>
2020-04-21 19:50:30 -0500

    Built pull part of ETL script

--- scripts/etl_clean_fields.py
+++ scripts/etl_clean_fields.py
@@ -2,7 +2,7 @@ import sys
 
 import pandas as pd
 import sqlalchemy
-import sqlalchemy.orm
+#import sqlalchemy.orm
 
 from context import config
 import solr
@@ -21,35 +21,15 @@ mysql_engine = sqlalchemy.create_engine(
 sobj = solr.Solr()
 sobj.setSolrURL('%s/select' % config.SOLR_ADDR)
 
-## Crossover testing
-
 am_id_df = pd.read_sql("SELECT db_id FROM article_metadata", con=mysql_engine)
-
-## Put IDs from MySQL into a SOLR query string
 ids = am_id_df['db_id'].tolist()
-ids_qclause = '"' + '" OR "'.join(ids) + '"'
-ids_q = 'id: (' + ids_qclause + ')'
 
-output_counts = {}
-
-output_counts['retrieved-articles'] = wglr.solr.getResultsFound(ids_q)
-
-print("Retrieving %d articles..." % output_counts['retrieved-articles'])
-
-import time
-t0    = time.time()
-docs  = wglr.solr.getDocuments(ids_q)
-test_time = time.time() - t0
-print("Loading time:  %0.3fs" % test_time)
-
+docs = sobj.getDocumentsFromIDs(ids)
 solr_df = pd.DataFrame(docs)
 
-#print '\n'.join([key + ':' + str(len(val))
-#                    if isinstance(val, list) 
-#                    else key + ':' + str(type(val)) 
-#                    for key, val in docs[3].iteritems()])
-print solr_df
-#print '\n'.join([doc[u'TITLE'] for doc in docs])
+etl_df = (solr_df
+          .filter(['id', 'PUBLICATION', 'DOCSOURCE'])
+          )
 
-output_counts['retrieved-articles-cleaned'] = solr_df.shape[0]
-print("Article count: %d" % solr_df.shape[0])
+print etl_df
+
