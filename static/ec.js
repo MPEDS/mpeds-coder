8a760cfc6e7e11fec1e5ac2312aeb48b24c4eaca
David Skalinder <skalinder@no.spam.wisc.edu>
2020-04-29 16:41:40 -0500

    Reverted revert 218368418 of article coding, fixed conflict in article-annotation-block.html by rescuing from current production version

--- static/ec.js
+++ static/ec.js
@@ -1,3 +1,40 @@
+/* Edit article-level info. */
+var modifyArticleAnnotations = function() {
+  var aid  = $(".article").attr("id").split("_")[1];
+  var pn   = $('#pass_number').val();
+
+  req = $.ajax({
+      type: "GET",
+      url:  $SCRIPT_ROOT + '/_load_article_annotation_block',
+      data: {
+        'article_id': aid,
+        'pn': pn
+      }
+  });
+
+  req.done(function() {
+    $("#flash-error").hide();
+
+    // add the article block to the HTML
+    $('#article-annotation-blocks').append(req.responseText);
+
+    // listeners for info radio buttons
+    $('#article-annotation-block :radio').change(selectRadio);
+
+    // listeners for text fields
+    $('#article-annotation-block :text').blur(storeArticleTextInput);
+    $('#article-annotation-block textarea').blur(storeArticleTextInput);
+
+    // listeners for adding or deleting checkboxes
+    $('#article-annotation-block :checkbox').change(selectArticleCheckbox);
+  });
+
+  req.fail(function(e) {
+    $("#flash-error").text("Error loading article annotation block.");
+    $("#flash-error").show();
+  });
+}
+
 /* Add or edit an event. Controls the event list. */
 var modifyEvent = function(e) {
   var aid  = $(".article").attr("id").split("_")[1];
@@ -72,7 +109,9 @@ var modifyEvent = function(e) {
     $('#basicinfo_block textarea').blur(storeText);
 
     // listeners for adding or deleting checkboxes
-    $(':checkbox').change(selectCheckbox);
+    $('#basicinfo_block :checkbox').change(selectCheckbox);
+    $('#yes-no_block :checkbox').change(selectCheckbox);
+    $('#preset_block :checkbox').change(selectCheckbox);
 
     // Get text select vars from DOM  
     $('.varblock').each(function() {
@@ -258,6 +297,9 @@ $(function(){
   // add event listener
   $('#add-event').click(modifyEvent);
 
+  // test code for article block
+  modifyArticleAnnotations()
+
   // mark done handler
   $('#mark-done').each(function() {
     $(this).click(function() {
@@ -283,4 +325,4 @@ $(function(){
     }); 
   });
 
-});    
+});
