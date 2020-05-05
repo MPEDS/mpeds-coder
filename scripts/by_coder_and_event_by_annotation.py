ce30b8d986ed8fed606c0fd203157468efcd0b52
David Skalinder <skalinder@no.spam.wisc.edu>
2020-05-05 15:38:30 -0500

    Added event text and time dfs, made db conn compatible with pandas 0.16.2

--- scripts/by_coder_and_event_by_annotation.py
+++ scripts/by_coder_and_event_by_annotation.py
@@ -8,35 +8,30 @@ from context import database
 from context import models
 
 
-## MySQL setup
-mysql_engine = sqlalchemy.create_engine(
-    'mysql://%s:%s@localhost/%s?unix_socket=%s&charset=%s' % 
-        (config.MYSQL_USER, 
-        config.MYSQL_PASS, 
-        config.MYSQL_DB, 
-        config.MYSQL_SOCK, 
-        'utf8'), 
-    convert_unicode=True)
-
+## Get tables into data frames
 coder_event_q = (database
 				 .db_session
          .query(models.CodeEventCreator)
          )
-eventdf = (pd
-           .read_sql_query(coder_event_q.statement, 
-                           coder_event_q.session.connection())
-           )
+eventdf = pd.read_sql_query(coder_event_q.statement, 
+                            # Old pandas fears real connections
+                            #coder_event_q.session.connection())
+                            coder_event_q.session.get_bind())
 
 coder_article_q = (database
                    .db_session
                    .query(models.CoderArticleAnnotation)
                    )
 articledf = pd.read_sql_query(coder_article_q.statement, 
-                            coder_article_q.session.connection())
+                              # Old pandas fears real connections
+                              #coder_event_q.session.connection())
+                              coder_event_q.session.get_bind())
 
+## Reshape tables
 e_val = (eventdf
          .filter(['coder_id', 'article_id', 'event_id', 'variable', 'value'])
          .dropna()
+         # Next block should remove both empty strings and missings
          .assign(length=lambda x: x.value.astype("str").str.len())
          .query('length > 0')
          .drop('length', axis=1)
@@ -44,9 +39,33 @@ e_val = (eventdf
          .unstack()
          )
 
+e_text = (eventdf
+          .filter(['coder_id', 'article_id', 'event_id', 'variable', 'text'])
+          .dropna()
+          # Next block should remove both empty strings and missings
+          .assign(length=lambda x: x.text.astype("str").str.len())
+          .query('length > 0')
+          .drop('length', axis=1)
+          .set_index(['coder_id', 'article_id', 'event_id', 'variable'])
+          .unstack()
+          )
+
+e_time = (eventdf
+          .filter(['coder_id', 'article_id', 'event_id', 'timestamp'])
+          .dropna()
+          .groupby(['coder_id', 'article_id', 'event_id'])
+          .agg(['min', 'max'])
+          #.set_index(['coder_id', 'article_id', 'event_id', 'variable'])
+          #.unstack()
+          )
+
 print "***** Event df *****"
 print eventdf
 print "***** Article df *****"
 print articledf
 print "***** Event value df *****"
 print e_val
+print "***** Event text df *****"
+print e_text
+print "***** Event time df *****"
+print e_time
