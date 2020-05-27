06c212dc0578fb051e7c82bd7eff06ba18a4f6c5
David Skalinder <skalinder@no.spam.wisc.edu>
2020-05-27 15:19:01 -0500

    Reindented grand unified join block

--- scripts/by_coder_and_event_by_annotation.py
+++ scripts/by_coder_and_event_by_annotation.py
@@ -157,24 +157,25 @@ def genByCoderAndEventByAnnotation(
     times_wide.columns = ['article_' + '_'.join(col).strip('_') 
                           for col in times_wide.columns.values]
     ## Grand Unified Merge
-    all_wide = (user
-                .merge(ca_wide, how='right', 
-                       left_on=['coder_id'],
-                       right_on=['article_coder_id'])
-                .drop(['article_coder_id'], axis=1)
-                .rename(columns={'article_article_id': 'article_id'})
-                .merge(e_wide, how='outer', 
-                       left_on=['coder_id', 'article_id'],
-                       right_on=['event_coder_id', 'event_article_id'])
-                .drop(['event_coder_id', 'event_article_id'], axis=1)
-                .rename(columns={'event_event_id': 'event_id'})
-                .merge(am, how='left', left_on='article_id', right_on='id')
-                .drop(['id'], axis=1)
-                .merge(times_wide, how='outer', 
-                       left_on=['coder_id', 'article_id'],
-                       right_on=['article_coder_id', 'article_article_id'])
-                .drop(['article_coder_id', 'article_article_id'], axis=1)
-                )
+    all_wide = (
+        user
+        .merge(ca_wide, how='right', 
+               left_on=['coder_id'],
+               right_on=['article_coder_id'])
+        .drop(['article_coder_id'], axis=1)
+        .rename(columns={'article_article_id': 'article_id'})
+        .merge(e_wide, how='outer', 
+               left_on=['coder_id', 'article_id'],
+               right_on=['event_coder_id', 'event_article_id'])
+        .drop(['event_coder_id', 'event_article_id'], axis=1)
+        .rename(columns={'event_event_id': 'event_id'})
+        .merge(am, how='left', left_on='article_id', right_on='id')
+        .drop(['id'], axis=1)
+        .merge(times_wide, how='outer', 
+               left_on=['coder_id', 'article_id'],
+               right_on=['article_coder_id', 'article_article_id'])
+        .drop(['article_coder_id', 'article_article_id'], axis=1)
+        )
 
     return all_wide
 
