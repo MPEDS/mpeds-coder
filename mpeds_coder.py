2db3744c5215972c48a2b806607da055e4ac4df0
David Skalinder <skalinder@no.spam.wisc.edu>
2020-06-03 17:38:45 -0500

    Remove extra commit() from changeCode()

--- mpeds_coder.py
+++ mpeds_coder.py
@@ -1338,7 +1338,6 @@ def changeCode(pn):
 
     for o in a:
         db_session.delete(o)
-    db_session.commit()
 
     ## add new value
     ec = CodeEventCreator(article, event, variable, value, current_user.id) 
