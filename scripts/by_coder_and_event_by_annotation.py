9dbf7eca1fd9d9ef1334d6b9f1037758fa71b934
David Skalinder <skalinder@no.spam.wisc.edu>
2021-04-16 12:31:22 -0500

    Get rid of article-level stuff and replace event-level stuff with function call

--- scripts/by_coder_and_event_by_annotation.py
+++ scripts/by_coder_and_event_by_annotation.py
@@ -129,28 +129,12 @@ def merge_pivot_times(
 def genByCoderAndEventByAnnotation(
         session,
         coder_event_table,
-        coder_article_table,
+#        coder_article_table,
         user_table,
         article_metadata_table):
 
     ## Get tables into data frames
-    event_q = (session
-               .query(coder_event_table)
-               .order_by('id')
-               )
-    event_long = pd.read_sql_query(event_q.statement, 
-                                   # Old pandas fears real connections
-                                   #event_q.session.connection())
-                                   event_q.session.get_bind())
-
-    ca_q = (session
-            .query(coder_article_table)
-            .order_by('id')
-            )
-    ca_long = pd.read_sql_query(ca_q.statement, 
-                                # Old pandas fears real connections
-                                #ca_q.session.connection())
-                                ca_q.session.get_bind())
+    event_long = query_to_pd(coder_event_table, session)
 
     u_q = (session
            .query(user_table.id.label('coder_id'), user_table.username)
@@ -176,100 +160,20 @@ def genByCoderAndEventByAnnotation(
                            #am_q.session.connection())
                            am_q.session.get_bind())
 
-    ## Concatenate duplicated variables
-    event_catted = catDuplicatedVariables(
-                     df=event_long,
-                     groupbylist=['coder_id', 'article_id', 
-                                 'event_id', 'variable'],
-                     valcollist=['value', 'text'])
-
-    ca_catted = catDuplicatedVariables(
-                     df=ca_long,
-                     groupbylist=['coder_id', 'article_id', 
-                                 'variable'],
-                     valcollist=['value', 'text'])
-
-    ## Reshape tables
-    e_val = unstackWithoutMissings(
-        df=event_catted,
-        indexlist=['coder_id', 'article_id', 'event_id', 'variable'],
-        valuecol='value')
-
-    e_text = unstackWithoutMissings(
-         df=event_catted,
-         indexlist=['coder_id', 'article_id', 'event_id', 'variable'],
-         valuecol='text')
-
-    ca_val = unstackWithoutMissings(
-         df=ca_catted,
-         indexlist=['coder_id', 'article_id', 'variable'],
-         valuecol='value')
-
-    ca_text = unstackWithoutMissings(
-          df=ca_catted,
-          indexlist=['coder_id', 'article_id', 'variable'],
-          valuecol='text')
-
-    ## Merge events and rename columns
-    e_wide = (e_val
-              .join(e_text, how='outer')
-              .reset_index()
-              .swaplevel(0, 1, axis=1)
-              )
-    e_wide.columns = ['event_' + '_'.join(col).strip('_') 
-                      for col in e_wide.columns.values]
-
-    ## Merge article annotations and rename columns
-    ca_wide = (ca_val
-               .join(ca_text, how='outer')
-               .reset_index()
-               .swaplevel(0, 1, axis=1)
-               )
-    ca_wide.columns = ['article_' + '_'.join(col).strip('_') 
-                      for col in ca_wide.columns.values]
-
-    ## Create df with max and min timestamps from both levels
-    ea_times = (event_long
-                .filter(['coder_id', 'article_id', 'timestamp'])
-                )
-
-    ca_times = (ca_long
-                .filter(['coder_id', 'article_id', 'timestamp'])
-                )
-
-    times = pd.concat([ea_times, ca_times])
-    times_wide = (times
-                  .dropna()
-                  .groupby(['coder_id', 'article_id'])
-                  .agg(['min', 'max'])
-                  .reset_index()
-                  .swaplevel(0, 1, axis=1)
-                  )
-    times_wide.columns = ['article_' + '_'.join(col).strip('_') 
-                          for col in times_wide.columns.values]
-
-    ## Align join key names
-    ca_wide = (ca_wide
-               .rename(columns={'article_coder_id': 'coder_id'})
-               .rename(columns={'article_article_id': 'article_id'})
-               )
-    e_wide = (e_wide
-              .rename(columns={'event_coder_id': 'coder_id'})
-              .rename(columns={'event_article_id': 'article_id'})
-              .rename(columns={'event_event_id': 'event_id'})
-              )
-    times_wide = (times_wide
-                  .rename(columns={'article_coder_id': 'coder_id'})
-                  .rename(columns={'article_article_id': 'article_id'})
-                  )
+    e_wide = pivot_annotations_wider(
+        event_long,
+        dim_cols=['coder_id', 'article_id', 'event_id'],
+        col_prefix='event')
+    times_wide = merge_pivot_times([event_long])
     am = (am
           .rename(columns={'id': 'article_id'})
           )
 
     ## Grand Unified Merge
     all_wide = (
-        ca_wide
-        .merge(e_wide, how='outer', on=['coder_id', 'article_id'])
+#        ca_wide
+#        .merge(e_wide, how='outer', on=['coder_id', 'article_id'])
+        e_wide
         .merge(times_wide, how='outer', on=['coder_id', 'article_id'])
         .merge(user, how='left', on=['coder_id'])
         .merge(am, how='left', on='article_id')
@@ -280,7 +184,7 @@ def genByCoderAndEventByAnnotation(
 export = genByCoderAndEventByAnnotation(
     session=database.db_session,
     coder_event_table=models.CodeEventCreator,
-    coder_article_table=models.CoderArticleAnnotation,
+    #coder_article_table=models.CoderArticleAnnotation,
     user_table=models.User,
     article_metadata_table=models.ArticleMetadata)
 
