8cb3290be260a5cfe34dcfcda98a2589268763f7
Alex Hanna <alex.hanna@gmail.com>
2021-08-04 06:29:59 -0500

    first draft of new models and tables for adjudication

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Boolean, Column, Date, DateTime, Integer, String, Unicode, ForeignKey, UniqueConstraint, Text, UnicodeText
+from sqlalchemy import Column, Date, DateTime, Integer, String, Unicode, ForeignKey, UniqueConstraint, Text, UnicodeText
 from sqlalchemy.orm import relationship, backref
 from flask_login import UserMixin
+from sqlalchemy.orm.relationships import RelationshipProperty
 from database import Base
 import datetime as dt
-import pytz
 from pytz import timezone
 
 central = timezone('US/Central')
@@ -28,6 +28,173 @@ class CoderArticleAnnotation(Base):
 
     def __repr__(self):
         return '<CoderArticleAnnotation %r>' % (self.id)
+
+
+class AdjudicatorEdit(Base):
+    __tablename__ = 'adjudicator_edit'
+    id        = Column(Integer, primary_key=True)
+    coder_id  = Column(Integer, ForeignKey('user.id'))
+    cec_id    = Column(Integer, ForeignKey('coder_event_creator.id'))
+    value     = Column(Text, nullable = False)
+    text      = Column(UnicodeText)
+    timestamp = Column(DateTime)
+
+    UniqueConstraint('coder_id', 'cec_id', name = 'unique1')
+
+    def __init__(self, coder_id, cec_id, value, text = None):
+        self.coder_id  = coder_id
+        self.cec_id    = cec_id
+        self.value     = value
+        self.text      = text
+        self.timestamp = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<AdjudicatorEdit %r>' % (self.id)
+
+
+class CanonicalEvent(Base):
+    __tablename__ = 'canonical_event'
+    id           = Column(Integer, primary_key=True)
+    coder_id     = Column(Integer, ForeignKey('user.id'))
+    key          = Column(Text)
+    notes        = Column(UnicodeText, nullable = False)
+    status       = Column(Text)
+    last_updated = Column(DateTime)
+
+    UniqueConstraint('key', name = 'unique1')
+
+    def __init__(self, coder_id, key, notes = None, status = 'In Progress'):
+        self.coder_id     = coder_id
+        self.key          = key
+        self.notes        = notes
+        self.status       = status
+        self.last_updated = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<CanonicalEvent %r>' % (self.id)
+
+
+class CanonicalEventRecord(Base):
+    __tablename__ = 'canonical_event_record'
+    id           = Column(Integer, primary_key=True)
+    coder_id     = Column(Integer, ForeignKey('user.id'))
+    canonical_id = Column(Integer, ForeignKey('canonical_event.id'))
+    cec_id       = Column(Integer, ForeignKey('coder_event_creator.id'))
+    timestamp    = Column(DateTime)
+
+    UniqueConstraint('canonical_id', 'cec_id', name = 'unique1')
+
+    def __init__(self, coder_id, canonical_id, cec_id):
+        self.coder_id     = coder_id
+        self.canonical_id = canonical_id
+        self.cec_id       = cec_id
+        self.timestamp    = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<CanonicalEventRecord %r>' % (self.id)
+
+
+class CanonicalEventRelationship(Base):
+    __tablename__ = 'canonical_event_relationship'
+    id                = Column(Integer, primary_key=True)
+    coder_id          = Column(Integer, ForeignKey('user.id'))
+    canonical_id1     = Column(Integer, ForeignKey('canonical_event.id'))
+    canonical_id2     = Column(Integer, ForeignKey('canonical_event.id'))
+    relationship_type = Column(Text)
+    timestamp         = Column(DateTime)
+
+    UniqueConstraint('canonical_id1', 'canonical_id2', 'relationship_type', name = 'unique1')
+
+    def __init__(self, coder_id, canonical_id1, canonical_id2, relationship_type):
+        self.coder_id          = coder_id
+        self.canonical_id1     = canonical_id1
+        self.canonical_id2     = canonical_id2
+        self.relationship_type = relationship_type
+        self.timestamp         = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<CanonicalEventRelationship %r -> %r (%r)>' % \
+            (self.canonical_id1, self.canonical_id2, self.relationship_type)
+
+
+class EventFlag(Base):
+    __tablename__ = 'event_flag'
+    id        = Column(Integer, primary_key=True)
+    coder_id  = Column(Integer, ForeignKey('user.id'))
+    event_id  = Column(Integer, ForeignKey('event.id'))
+    flag      = Column(Text)
+    timestamp = Column(DateTime)
+
+    UniqueConstraint('event_id', name = 'unique1')
+
+    def __init__(self, coder_id, event_id, flag):
+        self.coder_id  = coder_id
+        self.event_id  = event_id
+        self.flag      = flag
+        self.timestamp = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<EventFlag %r (%r)>' % (self.event_id, self.flag)
+
+
+class RecentCanonicalEvent(Base):
+    __tablename__ = 'recent_canonical_event'
+    id            = Column(Integer, primary_key=True)
+    coder_id      = Column(Integer, ForeignKey('user.id'))
+    canonical_id  = Column(Integer, ForeignKey('canonical_event.id'))
+    last_accessed = Column(DateTime)
+
+    UniqueConstraint('coder_id', 'canonical_id', name = 'unique1')
+
+    def __init__(self, coder_id, canonical_id):
+        self.coder_id      = coder_id
+        self.canonical_id  = canonical_id
+        self.last_accessed = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<RecentCanonicalEvent %r (%r)>' % (self.canonical_id, self.last_accessed)
+
+
+class RecentEvent(Base):
+    __tablename__ = 'recent_event'
+    id            = Column(Integer, primary_key=True)
+    coder_id      = Column(Integer, ForeignKey('user.id'))
+    event_id      = Column(Integer, ForeignKey('event.id'))
+    last_accessed = Column(DateTime)
+
+    UniqueConstraint('coder_id', 'event_id', name = 'unique1')
+
+    def __init__(self, coder_id, event_id):
+        self.coder_id      = coder_id
+        self.event_id      = event_id
+        self.last_accessed = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<RecentEvent %r (%r)>' % (self.event_id, self.last_accessed)
+
+
+class RecentSearch(Base):
+    __tablename__ = 'recent_search'
+    id            = Column(Integer, primary_key=True)
+    coder_id      = Column(Integer, ForeignKey('user.id'))
+    field         = Column(Text)
+    comparison    = Column(Text)
+    value         = Column(Text)
+    last_accessed = Column(DateTime)
+
+    UniqueConstraint('coder_id', 'field', 'comparison', 'value', name = 'unique1')
+
+    def __init__(self, coder_id, field, comparison, value):
+        self.coder_id      = coder_id
+        self.field         = field
+        self.comparison    = comparison
+        self.value         = value
+        self.last_accessed = dt.datetime.now(tz = central).replace(tzinfo = None)
+
+    def __repr__(self):
+        return '<RecentSearch %r-%r-%r (%r)>' % \
+            (self.field, self.comaprison, self.value, self.last_accessed)
+
 
 class CodeFirstPass(Base):
     __tablename__ = 'coder_first_pass'
@@ -219,11 +386,11 @@ class User(Base, UserMixin):
         self.password  = password
         self.authlevel = authlevel
 
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
+    # def get_id(self):
+    #     try:
+    #         return unicode(self.id)  # python 2
+    #     except NameError:
+    #         return str(self.id)  # python 3
 
     def __repr__(self):
         return '<Coder %r>' % (self.username)
