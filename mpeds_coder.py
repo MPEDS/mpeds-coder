d8faa4f0b7eb6fa7e3babab218c962c65b23b8ff
Alex Hanna <alex.hanna@gmail.com>
2021-08-26 12:57:23 -0700

    Update mpeds_coder.py

--- mpeds_coder.py
+++ mpeds_coder.py
@@ -41,7 +41,9 @@ from pytz import timezone
 import pytz
 
 ## flask
-from flask import Flask, request, session, g, redirect, url_for, abort, make_response, render_template, flash, jsonify, Response, stream_with_context
+from flask import Flask, request, session, g, redirect, url_for, abort, make_response, render_
+
+, flash, jsonify, Response, stream_with_context
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 ## jinja
@@ -83,10 +85,11 @@ app.config.from_pyfile('config.py')
 
 # customize template path
 # copy-pasta from https://stackoverflow.com/questions/13598363/how-to-dynamically-select-template-directory-to-be-used-in-flask
-template_loader = jinja2.ChoiceLoader([
-    jinja2.FileSystemLoader([app.config['ADDITIONAL_TEMPLATE_DIR']]),
-    app.jinja_loader])
-app.jinja_loader = template_loader
+if 'ADDITIONAL_TEMPLATE_DIR' in app.config:
+    template_loader = jinja2.ChoiceLoader([
+        jinja2.FileSystemLoader([app.config['ADDITIONAL_TEMPLATE_DIR']]),
+        app.jinja_loader])
+    app.jinja_loader = template_loader
 
 
 ## login stuff
